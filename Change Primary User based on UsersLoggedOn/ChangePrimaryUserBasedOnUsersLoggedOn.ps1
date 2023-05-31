984807a9c3bde36fbc6abfa265e25bd28d9955fd
alex <a.hurtado75@gmail.com>
2023-05-31 14:14:17 -0400

    Removed JWTToken

--- Change Primary User based on UsersLoggedOn/ChangePrimaryUserBasedOnUsersLoggedOn.ps1
+++ Change Primary User based on UsersLoggedOn/ChangePrimaryUserBasedOnUsersLoggedOn.ps1
@@ -19,7 +19,8 @@ Function Get-AccessToken{
     #  Authentication  #
     ####################
     If($token){
-        $tok = get-JWTDetails($token)
+        #$tok = get-JWTDetails($token)
+        $tok = MyJWTToken -token $token
         $numericDate = $tok.exp
         $epoch = New-Object DateTime 1970, 1, 1, 0, 0, 0, ([DateTimeKind]::Utc)
         $accessTokenExpiration = $epoch.AddSeconds($numericDate).ToLocalTime()
@@ -45,6 +46,51 @@ Function Get-AccessToken{
 
 }
 
+Function MyJWTToken{
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $True)]
+        [string]
+        $token
+    )
+    if (!$token.Contains(".") -or !$token.StartsWith("eyJ")) { Write-Error "Invalid token" -ErrorAction Stop }else{Write-Host "Good Token"}
+
+    # Token
+    foreach ($i in 0..1) {
+        $data = $token.Split('.')[$i].Replace('-', '+').Replace('_', '/')
+        switch ($data.Length % 4) {
+            0 { break }
+            2 { $data += '==' }
+            3 { $data += '=' }
+        }
+    }
+    $decodedToken = [System.Text.Encoding]::UTF8.GetString([convert]::FromBase64String($data)) | ConvertFrom-Json
+
+    # Signature
+    foreach ($i in 0..2) {
+        $sig = $token.Split('.')[$i].Replace('-', '+').Replace('_', '/')
+        switch ($sig.Length % 4) {
+            0 { break }
+            2 { $sig += '==' }
+            3 { $sig += '=' }
+        }
+    }
+    Write-Verbose "JWT Signature:"
+    Write-Verbose $sig
+    $decodedToken | Add-Member -Type NoteProperty -Name "sig" -Value $sig
+
+    # Convert Expiry time to PowerShell DateTime
+    $orig = (Get-Date -Year 1970 -Month 1 -Day 1 -hour 0 -Minute 0 -Second 0 -Millisecond 0)
+    $timeZone = Get-TimeZone
+    $utcTime = $orig.AddSeconds($decodedToken.exp)
+    $localTime = $utcTime.AddHours($timeZone.BaseUtcOffset.Hours)     # Return local time
+    $decodedToken | Add-Member -Type NoteProperty -Name "expiryDateTime" -Value $localTime
+
+    # Time to Expiry
+    $timeToExpiry = ($localTime - (get-date))
+    $decodedToken | Add-Member -Type NoteProperty -Name "timeToExpiry" -Value $timeToExpiry
+    Return $decodedToken
+}
 Function Get-MsGraphData($Path) {
     Get-AccessToken
     $FullUri = "https://graph.microsoft.com/beta/$Path"
