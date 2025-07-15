06cf92e13a3c41b102e1f5bc0235e234e720cc4c
alex <a.hurtado75@gmail.com>
2025-07-15 15:27:37 -0400

    disclaimer

--- Add devices to a group from csv serial number/Add_devices_to_EntraGroup_baseon_serialnumber.ps1
+++ Add devices to a group from csv serial number/Add_devices_to_EntraGroup_baseon_serialnumber.ps1
@@ -39,6 +39,25 @@
 #    [switch]$WhatIf
 #)
 
+###########################################################################
+# The sample scripts are not supported under any Microsoft standard support
+# program or service. The sample scripts are provided AS IS without warranty
+# of any kind. Microsoft further disclaims all implied warranties including,
+# without limitation, any implied warranties of merchantability or of fitness
+# for a particular purpose. The entire risk arising out of the use or
+# performance of the sample scripts and documentation remains with you. In no
+# event shall Microsoft, its authors, or anyone else involved in the creation,
+# production, or delivery of the scripts be liable for any damages whatsoever
+# (including, without limitation, damages for loss of business profits,
+# business interruption, loss of business information, or other pecuniary
+# loss) arising out of the use of or inability to use the sample scripts or
+# documentation, even if Microsoft has been advised of the possibility of such
+# damages.
+############################################################################
+
+
+
+
 #$CsvPath = "C:\temp\SerialNumbers.csv"
 #$SingleGroupId = "18cc3227-6342-40d4-b73d-3c33feb1096d"
 #$WhatIf = $false
